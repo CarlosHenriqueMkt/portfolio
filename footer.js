d11faeee4fb0e3c24288a018c905f9bf4e4d256e
carloshenriquemkt <falacom.ocarlos@gmail.com>
2024-06-24 11:49:31 -0300

    novas alterações

--- footer.js
+++ footer.js
@@ -10,3 +10,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
   contentDiv.appendChild(paragraph);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+  // Show the button when scrolling down 20px from the top of the document
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+  };
+
+  // Scroll to the top when the button is clicked
+  scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Smooth scroll
+    });
+  });
+});
